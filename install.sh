821d14aaa1182ffc9ebaab93c456eff8ffe8a3ac
Miguel Santos <ms8042939@gmail.com>
2024-10-20 12:17:52 +0100

    add more deps

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Copr repos
+sudo dnf copr enable solopasha/hyprland -y
+
 # Install deps
-sudo dnf install libnotify cargo dbus-devel pkgconf-pkg-config -y
+sudo dnf install libnotify cargo dbus-devel pkgconf-pkg-config hyprland xdg-desktop-portal-hyprland hyprland-plugins hyprland-autoname-workspaces hyprpaper hyprpicker hypridle hyprlock hyprshot waybar-git cliphist nwg-clipman waypaper hyprdim -y
 
 cargo install gnome-randr
 
