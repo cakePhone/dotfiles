e111085cf8e50ee8dca8342828ac6148a600faf5
Miguel Santos <ms8042939@gmail.com>
2025-05-05 19:55:51 +0100

    remove unused config

--- automation/starter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-exit
-
-notify-send "Running scripts" "Check ~/autostart_log.txt for logs" -a "Starter"
-
-cd ~/.config/automation/
-
-./auto_battery_optimisation.sh >> ~/autostart_log.txt 2>&1
-./download_sorter.sh >> ~/autostart_log.txt 2>&1
--- fish/config.fish
+++
@@ -1,3 +0,0 @@
-function fish_greeting
-  fastfetch -c ~/dotfiles/fastfetch/config.jsonc
-end
