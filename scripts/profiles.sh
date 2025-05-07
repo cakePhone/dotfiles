9777bf8eec644fc2df20fb0b35772994c3838742
Miguel Santos <ms8042939@gmail.com>
2025-05-07 14:57:45 +0100

    heavily rework power management

--- scripts/profiles.sh
+++ scripts/profiles.sh
@@ -1,28 +1,96 @@
 #!/bin/sh
-power_state=$(upower -i $(upower -e | grep BAT) | awk '/state/ {print $2}')
-current=$(sudo tuned-adm active | awk '{print $4}')
-if [ "$power_state" = "discharging" ]; then
-    canberra-gtk-play -i power-unplug
-    # On battery, always switch to powersave if not already
-    if [ "$current" != "powersave" ]; then
-        sudo tuned-adm profile powersave
-        hyprctl keyword monitor "eDP-1,1920x1080@60.32Hz,auto,1"
-        notify-send "Power Profile" "Switched to Power Saver mode (on battery)"
-    else 
-        notify-send "Power Profile" "Already in Power Saver mode (on battery)"
-    fi
-else
-    canberra-gtk-play -i power-plug
-    # On AC, toggle between performance and powersave
-    if [ "$current" = "throughput-performance" ]; then
-        sudo tuned-adm profile powersave
-        hyprctl keyword monitor "eDP-1,1920x1080@60.32Hz,auto,1"
-        notify-send "Power Profile" "Switched to Power Saver mode (on AC)"
-    else
-        sudo tuned-adm profile throughput-performance
-        hyprctl keyword monitor "eDP-1,1920x1080@144.42Hz,auto,1"
-        notify-send "Power Profile" "Switched to Performance mode (on AC)"
 
-    fi
+# Constants
+MONITOR_RES="1920x1080"
+MONITOR_ID="eDP-1"
+POWER_SAVE_HZ="60.32Hz"
+PERFORMANCE_HZ="144.42Hz"
+POWER_SAVE_PROFILE="powersave"
+PERFORMANCE_PROFILE="throughput-performance"
+POWER_UNPLUG_SOUND="power-unplug"
+POWER_PLUG_SOUND="power-plug"
+
+# Function to set monitor refresh rate
+set_monitor_rate() {
+  local refresh_rate=$1
+  hyprctl keyword monitor "$MONITOR_ID,$MONITOR_RES@${refresh_rate},auto,1"
+}
+
+# Function to set tuned profile and refresh rate
+set_profile() {
+  local profile=$1
+  local refresh_rate=$2
+  local message=$3
+
+  sudo tuned-adm profile "$profile"
+  set_monitor_rate "$refresh_rate"
+  notify-send "Power Profile" "$message"
+}
+
+# Toggle between powersave and performance mode
+toggle_mode() {
+  local current_profile=$(
+    sudo tuned-adm active | awk '{print $4}'
+  )
+
+  if [ "$current_profile" != "$POWER_SAVE_PROFILE" ]; then
+    set_profile \
+      "$POWER_SAVE_PROFILE" \
+      "$POWER_SAVE_HZ" \
+      "Switched to Power Saver mode"
+  else
+    set_profile \
+      "$PERFORMANCE_PROFILE" \
+      "$PERFORMANCE_HZ" \
+      "Switched to Performance mode"
+  fi
+}
+
+# Play a sound notification
+play_sound() {
+  local sound_event=$1
+  canberra-gtk-play -i "$sound_event"
+}
+
+# Main logic
+if [ "$1" = "true" ]; then
+  # On boot
+  local power_state=$(
+    upower -i "$(upower -e | grep BAT)" | awk '/state/ {print $2}'
+  )
+  local current_profile=$(
+    sudo tuned-adm active | awk '{print $4}'
+  )
+
+  if [ "$power_state" = "discharging" ] &&
+     [ "$current_profile" = "$PERFORMANCE_PROFILE" ]; then
+    set_profile \
+      "$POWER_SAVE_PROFILE" \
+      "$POWER_SAVE_HZ" \
+      "Switched to Power Saver mode"
+  else
+    set_profile \
+      "$PERFORMANCE_PROFILE" \
+      "$PERFORMANCE_HZ" \
+      "Switched to Performance mode"
+  fi
+else
+  # Not on boot
+  local power_state=$(
+    upower -i "$(upower -e | grep BAT)" | awk '/state/ {print $2}'
+  )
+
+  if [ "$power_state" = "discharging" ]; then
+    # On battery
+    play_sound "$POWER_UNPLUG_SOUND"
+    set_profile \
+      "$POWER_SAVE_PROFILE" \
+      "$POWER_SAVE_HZ" \
+      "Switched to Power Saver mode"
+  else
+    # On AC
+    play_sound "$POWER_PLUG_SOUND"
+    toggle_mode
+  fi
 fi
 
