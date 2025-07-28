21913cf232bb6e5d19976c409305696e7354610b
cakePhone <ms8042939@gmail.com>
2025-07-28 11:06:46 +0100

    add simple install script for arch

---
+++ install_arch.sh
@@ -0,0 +1,5 @@
+# make sure yay is installed
+sudo pacman -S --needed git base-devel yay
+
+# Install Hyprland and dependencies
+yay -S --noconfirm hyprland hyprpaper hyprpicker hypridle hyprlock hyprshot waybar-hyprland wlogout swaync fastfetch yazi btop ghostty zen-browser-bin zsh
