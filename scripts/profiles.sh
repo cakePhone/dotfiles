22b1052a3f348354e047978127ecb98781ef3e1b
cakePhone <ms8042939@gmail.com>
2025-07-28 11:07:48 +0100

    i did some scripts, alright?

--- scripts/profiles.sh
+++ scripts/profiles.sh
@@ -6,7 +6,7 @@ MONITOR_ID="eDP-1"
 POWER_SAVE_HZ="60.32Hz"
 PERFORMANCE_HZ="144.42Hz"
 POWER_SAVE_PROFILE="power-saver"
-PERFORMANCE_PROFILE="performance"
+PERFORMANCE_PROFILE="balanced"
 POWER_UNPLUG_SOUND="power-unplug"
 POWER_PLUG_SOUND="power-plug"
 
@@ -22,27 +22,24 @@ set_profile() {
     local refresh_rate=$2
     local message=$3
 
-    powerprofilesctl set "$profile"
     set_monitor_rate "$refresh_rate"
     notify-send "Power Profile" "$message"
 }
 
 # Toggle between powersave and performance mode
 toggle_mode() {
-    local current_profile=$(
-        powerprofilesctl get
-    )
+    local current_profile=$(tlp-stat --mode)
 
-    if [ "$current_profile" != "$POWER_SAVE_PROFILE" ]; then
+    if [ "$current_profile" = "battery" ]; then
         set_profile \
             "$POWER_SAVE_PROFILE" \
             "$POWER_SAVE_HZ" \
-            "Switched to Power Saver mode"
+            "Switched to $POWER_SAVE_PROFILE mode"
     else
         set_profile \
             "$PERFORMANCE_PROFILE" \
             "$PERFORMANCE_HZ" \
-            "Switched to Performance mode"
+            "Switched to $PERFORMANCE_PROFILE mode"
     fi
 }
 
@@ -58,19 +55,19 @@ if [ "$1" = "true" ]; then
     local power_state=$(
         upower -i "$(upower -e | grep BAT)" | awk '/state/ {print $2}'
     )
-    local current_profile=$(powerprofilesctl get)
+    local current_profile=$(tlp-stat --mode)
 
     if [ "$power_state" = "discharging" ] &&
-        [ "$current_profile" = "$PERFORMANCE_PROFILE" ]; then
+        [ "$current_profile" = "AC" ]; then
         set_profile \
             "$POWER_SAVE_PROFILE" \
             "$POWER_SAVE_HZ" \
-            "Switched to Power Saver mode"
+            "Switched to $POWER_SAVE_PROFILE mode"
     else
         set_profile \
             "$PERFORMANCE_PROFILE" \
             "$PERFORMANCE_HZ" \
-            "Switched to Performance mode"
+            "Switched to $PERFORMANCE_PROFILE mode"
     fi
 else
     # Not on boot
@@ -84,7 +81,7 @@ else
         set_profile \
             "$POWER_SAVE_PROFILE" \
             "$POWER_SAVE_HZ" \
-            "Switched to Power Saver mode"
+            "Switched to $POWER_SAVE_PROFILE mode"
     else
         # On AC
         play_sound "$POWER_PLUG_SOUND"
